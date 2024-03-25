ca02fcc21bd6006eca486bd18e647b4fc9cc8c53
Marco Rocci <marco.rocci92@gmail.com>
2024-03-25 16:12:04 +0100

    rimozione asset compilato

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,11 +5,7 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
 
 const mesi = (mese) => {
     switch(mese) {
