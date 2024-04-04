c64ba5ceea145c5a4b7fa41b68434267bc34a35d
Tibaldi3 <francesco.sbk3@gmail.com>
2024-04-04 09:34:05 +0200

    Funzionalità counter prezzo

--- public/scripts/counter.js
+++ public/scripts/counter.js
@@ -1,32 +1,58 @@
+document.querySelector("#piu").addEventListener("click", function() {
+    let numero = parseInt(document.querySelector("#qtninput").value);
+    numero++;
+    document.querySelector("#qtninput").value = numero;
+    document.querySelector("#qtninput").dispatchEvent(new Event("input"));
+});
+
+document.querySelector("#meno").addEventListener("click", function() {
+    let numero = parseInt(document.querySelector("#qtninput").value);
+    if (numero <= 1) return;
+    numero--;
+    document.querySelector("#qtninput").value = numero;
+    document.querySelector("#qtninput").dispatchEvent(new Event("input"));
+});
 
 
-document.querySelectorAll("#piu").forEach(e => {
-    e.addEventListener("click", function (e) {
-        let numero = e.target.parentElement.children[1].value
-        numero++
-        e.target.parentElement.children[1].value = numero
+document.querySelectorAll("#qtninput").forEach(e => {
+    e.addEventListener("input", ({target}) => {
+        const total_Price = document.getElementById("totalPrice")
+        const shipment = document.getElementById("shipment")
+        const n = target.value
+
+        
+        //Mostra il prezzo totale nella pagina
+        total_Price.textContent = `${(serverdata[0].total_price * n).toFixed(2)}€`;
+        shipment.textContent = `${
+            (Math.random() * 50).toFixed(2)
+        }€`;
+        var total_sum = parseFloat(total_Price.textContent.split("€")[0]) + parseFloat(shipment.textContent.split("€")[0]);
+        
+        document.getElementById("total").textContent = `${total_sum.toFixed(2)}€`;
     })
+
+
+    const total_Price = document.getElementById("totalPrice")
+    const shipment = document.getElementById("shipment")
+
+    
+    //Mostra il prezzo totale nella pagina
+    total_Price.textContent = `${serverdata[0].total_price}€`;
+    shipment.textContent = `${
+        (Math.random() * 50).toFixed(2)
+    }€`;
+    var total_sum = parseFloat(total_Price.textContent.split("€")[0]) + parseFloat(shipment.textContent.split("€")[0]);
+    
+    document.getElementById("total").textContent = `${total_sum.toFixed(2)}€`;
 })
-document.querySelectorAll("#meno").forEach(e => {
-    e.addEventListener("click", function (e) {
-        let numero = e.target.parentElement.children[1].value
-        if (numero <= 1) return
-        numero--
-        e.target.parentElement.children[1].value = numero
-    })
-})
+
+   
 
 
-const total_Price = document.getElementById("totalPrice")
-const shipment = document.getElementById("shipment")
 
-//Mostra il prezzo totale nella pagina
-total_Price.textContent = `${serverdata[0].total_price}€`;
-shipment.textContent = `${
-    (Math.random() * 50).toFixed(2)
-}€`;
-var total_sum = parseFloat(total_Price.textContent.split("€")[0]) + parseFloat(shipment.textContent.split("€")[0]);
 
-document.getElementById("total").textContent = `${total_sum.toFixed(2)}€`;
+
+
+
 
 
