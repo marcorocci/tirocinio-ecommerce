31a2123650c1547ccda45adeeb0e2045ceff1fb6
Marco Rocci <marco.rocci92@gmail.com>
2024-04-05 11:54:35 +0200

    query raw

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -5,19 +5,26 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use App\Repository\CheckoutRepository;
+//use App\Repository\CheckoutRepository;
 
 class CheckoutController extends AbstractController
 {
     private $checkoutRepository;
 
-    public function __construct(CheckoutRepository $checkoutRepository) {
-        $this->chekoutRepository = $checkoutRepository;
+    public function __construct() {
+        //$this->chekoutRepository = $checkoutRepository;
     }
     public function checkout() {
-        $result = $this->checkoutRepository->findAll();
+        $em = $this->getDoctrine()->getManager();
+        $sql = "select id, idProdotto from cart";
+        $resultSet = $em->getConnection()->executeQuery($sql);
+        $result = $resultSet->fetchAllAssociative();
+
+
+
+        //$result = $this->checkoutRepository->findAll();
         return $this->render("checkout/checkout.html.twig", [
-            'result' => $result
+            'result' => $result[0]
         ]);
     }
 }
