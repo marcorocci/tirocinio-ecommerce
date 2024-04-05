b83eb11747aa7a5a8caf444bd11c89469d871007
Martina <vespam5@outlook.it>
2024-04-05 11:36:09 +0200

    msg

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -5,19 +5,19 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
-use App\Repository\CartRepository;
+use App\Repository\CheckoutRepository;
 
 class CheckoutController extends AbstractController
 {
-    private $cartRepository;
+    private $checkoutRepository;
 
-    public function __construct(CartRepository $cartRepository) {
-        $this->cartRepository = $cartRepository;
+    public function __construct(CheckoutRepository $checkoutRepository) {
+        $this->chekoutRepository = $checkoutRepository;
     }
     public function checkout() {
-        $result = $this->cartRepository->findAll();
+        $result = $this->checkoutRepository->findAll();
         return $this->render("checkout/checkout.html.twig", [
-            'cartProd' => $result
+            'result' => $result
         ]);
     }
 }
---
+++ src/Repository/CheckoutRepository.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Checkout;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Checkout>
+ *
+ * @method Checkout|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Checkout|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Checkout[]    findAll()
+ * @method Checkout[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CheckoutRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Checkout::class);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function add(Checkout $entity, bool $flush = true): void
+    {
+        $this->_em->persist($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function remove(Checkout $entity, bool $flush = true): void
+    {
+        $this->_em->remove($entity);
+        if ($flush) {
+            $this->_em->flush();
+        }
+    }
+
+    public function findAll() {
+        $conn = $this->getEntityManager()->getConnection();
+
+        $sql = '
+            select u.nome, u.cognome, u.indirizzo_uno, u.indirizzo_due, u.regione, u.stato, u.codice_postale, p.carta_credito, p.carta_debito from checkout c inner join utenti u on c.id_utenti =u.id_utenti 
+            join pagamenti p on c.id_pagamenti = p.id_pagamenti;
+        
+            ';
+    
+        $stmt = $conn->prepare($sql);
+        $resultSet = $stmt->executeQuery();
+
+        // returns an array of arrays (i.e. a raw data set)
+        return $resultSet->fetchAllAssociative();
+    }
+
+    // /**
+    //  * @return Checkout[] Returns an array of Checkout objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Checkout
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
